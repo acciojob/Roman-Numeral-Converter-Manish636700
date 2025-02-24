8808ae4e86ab619e2e65383d3ab3dd2b91e6dccc
Manish Kumar Mittal <98324095+Manish636700@users.noreply.github.com>
2025-02-24 23:16:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -8,9 +8,16 @@ function convertToRoman(num) {
       5:['V', 5], 
       6:['I', 1]
     };
-
-  //your code here
-
+let result="";
+	for(let key in obj){
+		let[roman,value] = obj[key];
+			while(num>=value)
+				{
+					result += roman;
+						num-=value;
+				}
+	}
+	  return result;
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
